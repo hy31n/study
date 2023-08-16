73b2b338d5a99b633d7539a20803a96847ea633a
hyein <hy31n@naver.com>
2023-08-16 16:06:57 +0900

    prj

--- project/index.js
+++ project/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
@@ -17,6 +18,18 @@ const connection = mysql.createConnection(dbconfig);
 
 var mainRoute = require('./routes/mainRoute.js');
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'g7$In!2@S21Kc$5mB',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use(
   cors({
     origin: 'https://hyein.site',
@@ -25,6 +38,57 @@ app.use(
 );
 
 app.use('/', mainRoute);
+
+// 회원가입
+app.post('/signup', (req, res) => {
+  const { name, id, pw } = req.body;
+
+  const insertUserQuery =
+    'INSERT INTO member (memberid, membername, memberpassword) VALUES (?, ?, ?)';
+  connection.query(insertUserQuery, [id, name, pw], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.redirect('/login');
+  });
+});
+
+// 로그인
+app.post('/login', (req, res) => {
+  const { id, pw } = req.body;
+
+  const getUserQuery =
+    'SELECT * FROM member WHERE memberid = ? AND memberpassword = ?';
+  connection.query(getUserQuery, [id, pw], (err, results) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ message: '로그인 중 오류가 발생했습니다.' });
+    }
+
+    if (results.length > 0) {
+      req.session.user = results[0];
+
+      res.redirect('/');
+    } else {
+      res.redirect('/login');
+    }
+  });
+});
+
+// 로그아웃
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.clearCookie('user');
+      res.redirect('/');
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- project/js/header.js
+++ project/js/header.js
@@ -12,3 +12,16 @@ closeBtn.onclick = function () {
   closeBtn.classList.remove('active');
   searchBtn.classList.remove('active');
 };
+
+const userCookieExists = document.cookie.indexOf('user=') !== -1;
+const loginItem = document.querySelector('.loginItem');
+const loginLink = loginItem.querySelector('a');
+
+if (!userCookieExists) {
+  // 'user' 쿠키가 없는 경우
+  loginItem.style.display = 'list-item';
+  loginLink.href = '/login';
+} else {
+  loginLink.textContent = '로그아웃';
+  loginLink.href = '/logout';
+}
